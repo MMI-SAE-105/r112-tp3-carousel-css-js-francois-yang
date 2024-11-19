5fef79c517d316a174b82d781f3a6959be365d46
francois-yang <francois.yang@edu.univ-fcomte.fr>
2024-11-19 17:11:44 +0100

    Défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -4,16 +4,17 @@
 const carousel = document.querySelector("div>div");
 const prevButton = document.querySelector("button");
 const nextButton = document.querySelector("button+button");
-
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
 
 if (carousel) {
     // Scroll au clic sur le bouton précédent
     prevButton.addEventListener("click",(evt) => {
-        carousel.scrollBy({ left: -260, behavior: "smooth" }); ;
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" }); ;
     });
   
     // Scroll au clic sur le bouton suivant
     nextButton.addEventListener("click", (evt) => {
-        carousel.scrollBy({ left: +260, behavior: "smooth" });;
+        carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
     }); 
   }
